d512d1cdb9a84d0ad359d54fdc5ed20326d3057c
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-02-08 20:09:27 +0900

    filter function by regexp, suffix, public method

---
+++ function/filter.go
@@ -0,0 +1,46 @@
+package function
+
+import (
+	"regexp"
+	"fmt"
+	"strings"
+)
+
+// FilterBySuffix filters string matches regexp from given string slice.
+func FilterByRegexp(src []string, regex string) ([]string, error) {
+	r, err := regexp.Compile(regex)
+	if err != nil {
+		return nil, fmt.Errorf("failed to compile regexp. err: %s", err)
+	}
+
+	var ret []string
+	for _, s := range src {
+		if r.Match([]byte(s)) {
+			ret = append(ret, s)
+		}
+	}
+	return ret, nil
+}
+
+// FilterBySuffix filters string has suffix from given string slice.
+func FilterBySuffix(src []string, suffix string) []string {
+	var ret []string
+	for _, s := range src {
+		if strings.HasSuffix(s, suffix) {
+			ret = append(ret, s)
+		}
+	}
+	return ret
+}
+// FilterPublicMethod filters string starts with capital letter from given string slice.
+func FilterPublicMethod(src []string) []string {
+	var ret []string
+	for _, s := range src {
+		firstByte := byte(s[0])
+		// 0x41: A, 0x5a: Z
+		if 0x41 <= firstByte && firstByte <= 0x5a {
+			ret = append(ret, s) 
+		}
+	}
+	return ret
+}
--- main.go
+++ main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strings"
+	"flag"
 
 	"github.com/xruins/go-package-function-list/function"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalf("usage: %s directory", os.Args[0])
-	}
-
-	dir := os.Args[1]
-	fnames, err := function.ParseDir(dir)
+	dir := flag.String("dir", "", "directory to parse")
+	regex := flag.String("regex", "", "regexp to filter results")
+	suffix := flag.String("suffix", "", "if specified, show only functions has given suffix")
+	delimiter := flag.String("delimiter", " ", "delimiter among function names")
+	publicOnly := flag.Bool("public-only", false, "whether shows only public methods or not")
+	flag.Parse()
+	
+	fnames, err := function.ParseDir(*dir)
 	if err != nil {
 		fmt.Println("Failed to parse package:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(strings.Join(fnames, ","))
+	if *regex != "" {
+		newFnames, err := function.FilterByRegexp(fnames, *regex)
+		if err != nil {
+			fmt.Println("Failed to parse regexp: ", err)
+			os.Exit(1)
+		}
+		fnames = newFnames
+	}
+	if *suffix != "" {
+		fnames = function.FilterBySuffix(fnames, *suffix)
+	}
+	if *publicOnly {
+		fnames = function.FilterPublicMethod(fnames)
+	}
+
+	fmt.Println(strings.Join(fnames, *delimiter))
 }
