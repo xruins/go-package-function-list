cc704cc0254ac028bb02bd6c84668044b6a9d475
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-04-15 17:19:14 +0900

    get rid of redundant defaults

--- main.go
+++ main.go
@@ -5,19 +5,18 @@ import (
 	"os"
 	"strings"
 
-	_ "github.com/jessevdk/go-flags"
+	flags "github.com/jessevdk/go-flags"
 	"github.com/xruins/go-package-function-list/function"
 )
 
 // CmdOptions represents command-line options for go-package-function-list
 type cmdOptions struct {
-	Dir        string `short:"d" required:"true"`
-	Regex      string `short:"x" default:"" description:"directory to parse"`
-	Prefix     string `short:"p" default:"" description:"a prefix to filter functions"`
-	Suffix     string `short:"s" default:"" description:"a suffix to filter functions"`
+	Regex      string `short:"x" description:"regexp to filter functions"`
+	Prefix     string `short:"p" description:"a prefix to filter functions"`
+	Suffix     string `short:"s" description:"a suffix to filter functions"`
 	Delimiter  string `short:"d" default:" " description:"delimiter among function names"`
-	PublicOnly bool   `short:"o" default:false description:"whether shows only public methods or not"`
-	Recursive  bool   `short:"r" default:false description:"parses directory recursively if true"`
+	PublicOnly bool   `short:"o" description:"whether shows only public methods or not"`
+	Recursive  bool   `short:"r" description:"parses directory recursively if true"`
 }
 
 func main() {
